045efc9640ce7bffa5a64d79e6dc7122ff822fd2
codinlog <codinlog@foxmail.com>
2021-10-04 18:37:41 +0800

    添加规范代码

--- Chapter1/CodeStyleGuide.java
+++ Chapter1/CodeStyleGuide.java
@@ -211,8 +211,7 @@ class UserProxy {
  * 注释。尽量不要在接口里定义变量，如果一定要定义变量，肯定是 与接口方法相关，并且是整个应用的基础常量。
  */
 
-// ! 是否应在接口加上前缀I?
-interface IProxyFactory {
+interface ProxyFactory {
     static final String PROXY_NAME = "xxxxxxx";
 
     void fn();
@@ -251,7 +250,8 @@ abstract class AbstractTranslator implements Translatable {
  * 枚举类名建议带上 Enum 后缀，枚举成员名称需要全大写，单词间用下划线隔开。
  */
 enum DealStatusEnum {
-    SUCCESS, UNKOWN_REASON
+    SUCCESS,
+    UNKOWN_REASON,
 }
 /***************************************** */
 /**
@@ -262,5 +262,95 @@ enum DealStatusEnum {
  * 不允许出现任何魔法值（即未经定义的常量）直接出现在代码中
  */
 
- 
+class ConstVariableDefine {
+    ConstVariableDefine(String consts){
+        switch (consts) {
+            case "A": {
+
+            }
+            break;
+            case "B": {
+
+            }
+            break;
+            default: {
+
+            }
+            break;
+        }
+    }
+
+    /**
+     * long或者 Long初始赋值时，必须使用大写的 L，不能是小写的
+     */
+    private long longNum = 123456L;
+    /**
+     * 常量功能进行归类，分开维护。如：缓存：CacheConsts下；系统配置：ConfigConsts下。
+     * 常量的复用层次有五层：1.跨应用共享常量、2.应用内共享常量、3.子工程内共享常量、4.包 内共享常量、5.类内共享常量。
+     *  1） 跨应用共享常量：放置在二方库中，通常是 client.jar中的 constant目录下。
+     *  2） 应用内共享常量：放置在一方库的 modules中的constant目录下。
+     *  3） 子工程内部共享常量：即在当前子工程的 constant目录下。
+     *  4） 包内共享常量：即在当前包下单独的 constant目录下。
+     *  5） 类内共享常量：直接在类内部 private static final定义。
+     */
+    /**
+     * 如果变量值仅在一个范围内变化用 Enum类。如果还带有名称之外的延伸属性，必须 使用 Enum类
+     */
+    public enum WeekEnum{ MONDAY(1), TUESDAY(2), WEDNESDAY(3), THURSDAY(4), FRIDAY(5), SATURDAY(6), SUNDAY(7);}
+}
+/***************************************** */
+
+/**
+ * 3. 格式规约
+ */
+
+ class CodeFormatStyle{
+    public static void main(String args[]){
+        // 缩进 4 个空格
+        String say = "hello";
+        // 运算符的左右必须有一个空格
+        int flag = 0;
+        // 关键词 if 与括号之间必须有一个空格，括号内的 f 与左括号，0 与右括号不需要空格
+        if (flag == 0) {
+            System.out.println(say);
+        }
+        if (flag == 1) {
+            System.out.println("world");
+            // 左大括号前加空格且不换行；左大括号后换行 if (flag == 1) {
+            System.out.println("world");
+            // 右大括号前换行，右大括号后有 else，不用换行
+        } else {
+            System.out.println("ok");
+            // 在右大括号后直接结束，则必须换行
+        }
+        /**
+         * 单行字符数限制不超过 120 个，超出需要换行，换行时遵循如下原则：
+         * 1） 第二行相对第一行缩进 4 个空格，从第三行开始，不再继续缩进，参考示例。
+         * 2） 运算符与下文一起换行。
+         * 3） 方法调用的点符号与下文一起换行。
+         * 4） 在多个参数超长，逗号后进行换行。
+         * 5） 在括号前不要换行。
+         */
+        StringBuffer sb = new StringBuffer();
+        //超过 120 个字符的情况下，换行缩进 4 个空格，并且方法前的点符号一起换行
+        sb.append("zi")
+            .append("xin")
+            .append("huang")
+            .append("huang")
+            .append("huang");
+        // 方法参数在定义和传入时，多个参数逗号后边必须加空格。
+        method("a", "b", "c");
+        // 文件编码设置为 UTF-8; 换行符使用 Unix格式，
+        // 没有必要增加若干空格来使某一行的字符与上一行的相应字符对齐。
+        int a = 3;
+        long b = 4L;
+        float c = 5F;
+        StringBuffer sb1 = new StringBuffer();
+        // 方法体内的执行语句组、变量的定义语句组、不同的业务逻辑之间或者不同的语义 之间插入一个空行。相同业务逻辑和语义之间不需要插入空行。
+    }
+
+    static void method(String a, String b, String c){
+
+    }
+ }
 /***************************************** */
