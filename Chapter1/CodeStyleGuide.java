7eceedd27e74dc8b4fc44f1dd8e51049f2583e44
codinlog <codinlog@foxmail.com>
2021-10-12 22:33:36 +0800

    添加

--- Chapter1/CodeStyleGuide.java
+++ Chapter1/CodeStyleGuide.java
@@ -923,6 +923,13 @@ class CollectionSample{
         Executors.newScheduledThreadPool(12);
         Executors.newCachedThreadPool();
         Executors.newSingleThreadScheduledExecutor();
+        //!ArrayBlockingQueue ：一个由数组结构组成的有界阻塞队列
+        //!LinkedBlockingQueue ：一个由链表结构组成的有界阻塞队列（常用）
+        //!PriorityBlockingQueue ：一个支持优先级排序的无界阻塞队列
+        //!DelayQueue： 一个使用优先级队列实现的无界阻塞队列
+        //!SynchronousQueue： 一个不存储元素的阻塞队列（常用）
+        //!LinkedTransferQueue： 一个由链表结构组成的无界阻塞队列
+        //!LinkedBlockingDeque： 一个由链表结构组成的双向阻塞队列
         /**
          * 线程资源必须通过线程池提供，不允许在应用中自行显式创建线程。
          * ThreadPoolExecutor
